ab22c364f160efd4232a4f85c5e15482c4ce03d2
anugrahaa <vr.anugrahaa@gmail.com>
2015-12-12 22:39:02 +0530

    prescription yo!

--- view/doctor/prescription.php
+++ view/doctor/prescription.php
@@ -1,45 +1,54 @@
 <?php require_once('/var/www/html/GH/imports.php');
 ?>
 <body class="teal lighten-3">
-<div class = "row">
- <div class = 'col s6 offset-s1'   >
-<div class = "cardpanel">
-    <div class="container ">
-        <table id="myTable">
-            <thead>
-            <tr>
-                <th data-field="med">Medicine</th>
-                <th data-field="dosage">Dosage</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td><input id="med1"  type="text" tag ="med"required></td>
-                <td><input id="dose1" type="text" tag ="dose" required></td>
-                <td><a class="waves-effect waves-light btn" onclick=to_add_new()><i class="material-icons left">add</i></a></td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-
-</div>
-</div>
-<div class = 'col s3 offset-s1'>
-    <div class = "card-panel">
-        <div class = "container">
-           <textarea id="diag" rows="25" columns="50"></textarea>
+<div class="container">
+<div class="row">
+    <div class = 'col s12'>
+        <div class = "card-panel" style="height: 200px;">
+                <textarea id="diag" name="diag" rows="50" max-width="50" placeholder="Enter Diagnosis here" style="height: 165px;resize: none;"required></textarea>
         </div>
     </div>
+ <div class="col s12"  >
+     <div class = "card-panel">
+                <table id="myTable">
+                    <thead>
+                    <tr>
+                        <th data-field="med">Medicine</th>
+                        <th data-field="dosage">Dosage</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><input id="med1"  type="text" tag ="med"required></td>
+                        <td><input id="dose1" type="text" tag ="dose" required></td>
+                    </tr>
+                    <tr>
+                        <td><input id="med2"  type="text" tag ="med"required></td>
+                        <td><input id="dose2" type="text" tag ="dose" required></td>
+                    </tr>
+                    <tr>
+                        <td><input id="med3"  type="text" tag ="med"required></td>
+                        <td><input id="dose3" type="text" tag ="dose" required></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+  </div>
 </div>
-</div>
-<div class="row">
-    <div class = "col s1 offest-s11">
-        <a class="waves-effect waves-light btn" onclick=on_save() type="submit" name="save">SAVE</a></td>
+
+<div class = "row">
+    <div class = "col s12 offset-s1">
+        <a class="waves-effect waves-light btn" onclick=on_save()
+           type="submit" name="save">SAVE</a>
+        <a class="waves-effect waves-light btn" onclick=to_add_new()><i class="material-icons left">add</i>Add Medicines</a>
     </div>
 </div>
+
 </body>
+
 <script>
-    var i=2;
+    var i=4;
 
     function to_add_new(){
     //add new row with id incrementes as medi and dosei where i starts from 2
@@ -48,7 +57,7 @@
     }
 
     function on_save(){
-        var x=[], y=[];ipti
+        var x=[], y=[];
         var j;
         var z;
         for(j=1;j<=i;j++){
